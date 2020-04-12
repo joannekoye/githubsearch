c90ae51b388744dca59fc63087c57d528e11f0be
Joan Nekoye <joannekoye@gmail.com>
2020-04-12 13:50:48 +0300

    add user and repository classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
@@ -12,7 +13,8 @@ import { LandingpageComponent } from './landingpage/landingpage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
